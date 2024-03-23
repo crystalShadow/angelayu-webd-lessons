0215f9aa40690e551a2f0ea91749cb4c4364dae6
crystalShadow <shashankshekhar1299@gmail.com>
2024-03-23 21:26:49 +0530

    steps-to-create-a-express-server

---
+++ Express/less1-express-server/index.js
@@ -0,0 +1,8 @@
+//Six steps to create a server
+//Step 1 : Create directory
+//Step 2 : Create index.js file
+//Step 3 : initialise npm
+//Step 4 : Install the express package
+//Step 5 : Write Server application in index.js
+//Step 6 : Start Server
+
