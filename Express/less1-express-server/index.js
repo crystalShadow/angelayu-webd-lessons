10c791c1a3c8070e2075ce8dcaaa87e8ed858670
crystalShadow <shashankshekhar1299@gmail.com>
2024-03-23 22:44:37 +0530

    created and tested first example server

--- Express/less1-express-server/index.js
+++ Express/less1-express-server/index.js
@@ -5,6 +5,20 @@
 //Step 4 : Install the express package
 //Step 5 : Write Server application in index.js
 //Step 6 : Start Server
-import express from {express};
+import express from 'express';
 
 //testing git add
+
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  });
+  
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  });
+
+  //This creates a server which keeps listening on port 3000 and when we hit 3000 on the specified route. Server responds "Hello World"
